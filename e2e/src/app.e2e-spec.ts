104926684da545f6c68cc7c55721aab9f9487917
wanghao <muyao1987@qq.com>
2021-02-05 14:59:48 +0800

    升级到V3.0

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to marsgis-cesium-ng!');
+    expect(page.getTitleText()).toEqual('Welcome to mars3d-ng!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'marsgis-cesium-ng'`, () => {
+  it(`should have as title 'mars3d-ng'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('marsgis-cesium-ng');
+    expect(app.title).toEqual('mars3d-ng');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to marsgis-cesium-ng!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mars3d-ng!');
   });
 });
