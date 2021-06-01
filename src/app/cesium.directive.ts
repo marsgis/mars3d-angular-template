85a316a85d0140fd1ab53b99e00e6b66bd3a3bf8
wanghao <muyao1987@qq.com>
2021-06-01 14:07:04 +0800

    移除mars3d-esri

--- src/app/cesium.directive.ts
+++ src/app/cesium.directive.ts
@@ -5,8 +5,8 @@ import { Directive, ElementRef, OnInit } from '@angular/core';
 import * as mars3d from 'mars3d';
 
 // 导入插件(其他插件类似，插件清单访问：http://mars3d.cn/dev/guide/start/install.html)
-// arcgis wfs服务插件
-import 'mars3d-esri'
+// echarts插件
+// import 'mars3d-echarts'
 
 // 导航球插件
 // import 'mars3d-navigation/dist/mars3d-navigation.css'//已在src\styles.less引入
